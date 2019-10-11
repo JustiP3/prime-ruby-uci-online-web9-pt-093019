42a60c3d98d2ffdaded876fe308c79135c910aef
Justin Phillips <justinphil02@yahoo.com>
2019-10-11 21:58:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,11 +5,8 @@ if int < 0
   int = int * -1 
 end 
 
-range = (1..int).to_a 
+range = (2..int).to_a 
 range.pop
-range.shift 
-range2 = range 
-prime = true 
 
 
 if range.length == 1 
@@ -18,7 +15,7 @@ end
 
 
   
-end
+
 
 return prime  
 
